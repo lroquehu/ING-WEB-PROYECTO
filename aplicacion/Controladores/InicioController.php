d7eba9adf25ed2c5853680fdfd2848ae94b1bdd1
Luis Roque Huacca <lroque@dcp.pe>
2025-10-09 09:41:05 -0500

    lógica para obtener los productos destacados

--- aplicacion/Controladores/InicioController.php
+++ aplicacion/Controladores/InicioController.php
@@ -1,1 +1,10 @@
- 
+<?php
+    class InicioController {
+        public function index() {
+            // Lógica para obtener productos destacados
+            $productos = []; 
+            
+            include 'aplicacion/Vistas/inicio/index.php';
+        }
+    }
+?>
