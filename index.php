97ddfcdb56d5732cc2cbb956cac94671b7e53359
Luis Roque Huacca <lroque@dcp.pe>
2025-10-09 09:03:39 -0500

    logica de enrutamiento basica

--- index.php
+++ index.php
@@ -1,1 +1,23 @@
  
+<?php
+require_once 'rutas.php';
+require_once 'aplicacion/Configuracion/BaseDatos.php';
+
+$controller = $_GET['c'] ?? 'Inicio';
+$action = $_GET['a'] ?? 'index';
+
+$controllerClass = $controller . 'Controller';
+$controllerFile = "aplicacion/Controladores/{$controllerClass}.php";
+
+if (file_exists($controllerFile)) {
+    require_once $controllerFile;
+    $controllerInstance = new $controllerClass();
+    if (method_exists($controllerInstance, $action)) {
+        $controllerInstance->$action();
+    } else {
+        echo "Método no encontrado";
+    }
+} else {
+    echo "Controlador no encontrado";
+}
+?>
