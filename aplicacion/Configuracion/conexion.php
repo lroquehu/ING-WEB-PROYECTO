490fb03392d88778e60b5cdacd6e93eb0f9f9f22
lroquehu <luisroque299792458@gmail.com>
2025-11-19 09:12:10 -0500

    conexion a db de azure

--- aplicacion/Configuracion/conexion.php
+++ aplicacion/Configuracion/conexion.php
@@ -1,7 +1,7 @@
 <?php
 // PHP Data Objects(PDO) Sample Code:
 try {
-    $conn = new PDO("sqlsrv:Server = tcp:uniemprende-server.database.windows.net,1433; Database = uniemprendeDB", "adminsql", "<Loscapis>");
+    $conn = new PDO("sqlsrv:server = tcp:uniemprende-server.database.windows.net; Database = uniemprendeDB", "adminsql", "<Loscapis>");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch (PDOException $e) {
@@ -11,6 +11,6 @@ catch (PDOException $e) {
 
 // SQL Server Extension Sample Code:
 $connectionInfo = array("UID" => "adminsql", "pwd" => "<Loscapis>", "Database" => "uniemprendeDB", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-$serverName = "tcp:uniemprende-server.database.windows.net,1433";
+$serverName = "tcp:uniemprende-server.database.windows.net";
 $conn = sqlsrv_connect($serverName, $connectionInfo);
 ?>
