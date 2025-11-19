e741e73f4d4cb5f7162a51b10e71daf04e722715
lqueanin <107727252+lqueanin@users.noreply.github.com>
2025-11-19 11:35:41 -0500

    Update conexion.php

--- aplicacion/Configuracion/conexion.php
+++ aplicacion/Configuracion/conexion.php
@@ -1,16 +1,23 @@
 <?php
-// PHP Data Objects(PDO) Sample Code:
-try {
-    $conn = new PDO("sqlsrv:server = tcp:uniemprende-server.database.windows.net; Database = uniemprendeDB", "adminsql", "<Loscapis>");
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch (PDOException $e) {
-    print("Error connecting to SQL Server.");
-    die(print_r($e));
-}
+    class Conexion {
+        private $server   = "locahost,1433";
+        private $database = "uniemprendeDB";
+        private $username = "sa";
+        private $password = "Lorenz119013";
+        private $conn;
 
-// SQL Server Extension Sample Code:
-$connectionInfo = array("UID" => "adminsql", "pwd" => "<Loscapis>", "Database" => "uniemprendeDB", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-$serverName = "tcp:uniemprende-server.database.windows.net";
-$conn = sqlsrv_connect($serverName, $connectionInfo);
+        public function conectar() {
+            try {
+                $dsn = "sqlsrv:Server={$this->server};Database={$this->database};TrustServerCertificate=true";
+                $options = [
+                    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+                ];
+                return new PDO($dsn, $this->username, $this->password, $options);
+
+            } catch (PDOException $e) {
+                error_log("Error SQL Azure: " . $e->getMessage());
+                die($e->getMessage());
+            }
+        }
+    }
 ?>
